5278414df6cdd26fab33535b4a5ba2882e9cbcb2
henriquemtn <henriquemartinsilveira2@gmail.com>
2024-02-16 09:05:13 -0300

    add banner responsive

--- src/layout/banner.tsx
+++ src/layout/banner.tsx
@@ -11,15 +11,15 @@ export default function Banner() {
 
     return (
         <div id='home' className="bg-[#69825F] w-full h-[717px] flex md:justify-center items-center">
-        <div className='flex justify-between items-center  md:w-[1150px] text-[20px] -92'>
-                <div className="w-[500px] mr-2 text-left md:pr-10"> 
+        <div className='flex justify-between flex-col-reverse gap-5 md:gap-0 md:flex-row items-center  md:w-[1150px] text-[20px] -92'>
+                <div className="w-full md:w-[500px] md:mr-2 text-center md:text-left md:pr-10"> 
                     <FadeInOnScroll>
                         <h1 className='text-white'>“A psicoterapia propõe te levar ao protagonismo da própria história.”</h1>
                         <p className='text-white font-light pb-4'>Lisandra Crespi</p>
                         <button onClick={handleClick} className='p-4 bg-white rounded-md px-16 font-light text-[#69825F]'>Clique Aqui</button>
                     </FadeInOnScroll>
                 </div>
-                <div className='bg-white h-[480px] w-[380px] flex justify-center items-center rounded-t-full'>
+                <div className='bg-white h-[480px] w-[280px] md:w-[380px] flex justify-center items-center rounded-t-full'>
                     <img src={cone} alt="logo" />
                 </div>
             </div>
